a7d169aa24d1984ebddd728aa55bbcd56283d54f
Ishan Sharma <ishansharma1320@gmail.com>
2025-09-15 18:37:43 -0700

    Added Stage 02 Prepare Base Model Pipeline & its components

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from cnnClassifier.pipeline.stage_02_prepare_base_model import BaseModelPreparationPipeline
 
 try:
         STAGE_NAME = "Data Ingestion Stage"
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+try:
+        STAGE_NAME = "Base Model Preparation Stage"
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = BaseModelPreparationPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
